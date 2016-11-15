314d2cd5e642196a687851ed7a37999752efd1e7
Gede Lumbung <gedesumawijaya@gmail.com>
2016-11-16 05:37:14 +0800

    fetch pegawai

--- app/Model/Pegawai.php
+++ app/Model/Pegawai.php
@@ -7,8 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Pegawai extends Model
 {
     protected $table = 'pegawai';
-    
+
     protected $casts = [
         'features' => 'json'
     ];
+
+    public function unit_kerja()
+    {
+        return $this->belongsTo('Simpeg\Model\UnitKerja');
+    }
 }
--- resources/views/backend/pegawai/index.blade.php
+++ resources/views/backend/pegawai/index.blade.php
@@ -22,6 +22,26 @@
 					<a href="{{route('dashboard.pegawai.add')}}" class="btn btn-sm btn-primary">Add New</a>
 				</th>
 			</tr>
+			<tbody>
+				@foreach($pegawai as $key => $data)
+				<tr>
+					<td>{{$data->nip}}</td>
+					<td>{{$data->nama}}</td>
+					<td>{{$data->unit_kerja->title}}</td>
+					<td>{{$data->jenis_jabatan}}</td>
+					<td>{{$data->nama_jabatan}}</td>
+					<td>50%</td>
+					<td>
+	        			<a href="{{ route('dashboard.golongan.edit', ['id' => $data->id]) }}">
+	        				<i class="glyphicon glyphicon-pencil text-muted"></i>
+	        			</a>
+	        			<a href="{{ route('dashboard.golongan.delete', ['id' => $data->id]) }}">
+	        				<i class="glyphicon glyphicon-trash text-danger"></i>
+	        			</a>
+					</td>
+				</tr>
+				@endforeach
+			</tbody>
 		</table>
 	</div>
 </div>
