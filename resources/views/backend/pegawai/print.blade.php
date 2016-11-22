8d22b063b1f05fd43962a2ebb0e817a8dd198ba5
Gede Lumbung <gedesumawijaya@gmail.com>
2016-11-23 02:42:49 +0800

    fix print pegawai

--- resources/views/backend/pegawai/print.blade.php
+++ resources/views/backend/pegawai/print.blade.php
@@ -182,6 +182,521 @@
 	<br>
 	<h4><b>III. DATA KELUARGA </b></h4>
 
+	<table border="1">
+		<tr>
+			<td colspan="3">
+				<b>
+					AYAH KANDUNG
+				</b>
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				1.
+			</td>
+			<td>
+				Nama
+			</td>
+			<td>
+			 	{{$ayah->nama}} 
+			</td>
+		</tr>
+		<tr>
+			<td>
+				2.
+			</td>
+			<td>
+				Gelar Depan
+			</td>
+			<td>
+				{{$ayah->gelar_depan}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				3.
+			</td>
+			<td>
+				Gelar Belakang
+			</td>
+			<td>
+				{{$ayah->gelar_belakang}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				4.
+			</td>
+			<td>
+				Tanggal Lahir
+			</td>
+			<td>
+				{{indonesian_date($ayah->tanggal_lahir)}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				5.
+			</td>
+			<td>
+				Agama
+			</td>
+			<td>
+				{{ucwords($ayah->agama)}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				6.
+			</td>
+			<td>
+				Email
+			</td>
+			<td>
+				{{$ayah->email}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				7.
+			</td>
+			<td>
+				Status Perkawinan
+			</td>
+			<td>
+				{{$ayah->status_perkawinan}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				8.
+			</td>
+			<td>
+				Status Hidup
+			</td>
+			<td>
+				{{$ayah->status_hidup}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				9.
+			</td>
+			<td>
+				NO HP / TELPON
+			</td>
+			<td>
+				{{$ayah->telepon}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				10.
+			</td>
+			<td>
+				Alamat
+			</td>
+			<td>
+				{{$ayah->alamat}}
+			</td>
+		</tr>
+		<tr>
+			<td colspan="3">
+				<b>
+					IBU KANDUNG
+				</b>
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				1.
+			</td>
+			<td>
+				Nama
+			</td>
+			<td>
+			 	{{$ibu->nama}} 
+			</td>
+		</tr>
+		<tr>
+			<td>
+				2.
+			</td>
+			<td>
+				Gelar Depan
+			</td>
+			<td>
+				{{$ibu->gelar_depan}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				3.
+			</td>
+			<td>
+				Gelar Belakang
+			</td>
+			<td>
+				{{$ibu->gelar_belakang}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				4.
+			</td>
+			<td>
+				Tanggal Lahir
+			</td>
+			<td>
+				{{indonesian_date($ibu->tanggal_lahir)}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				5.
+			</td>
+			<td>
+				Agama
+			</td>
+			<td>
+				{{ucwords($ibu->agama)}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				6.
+			</td>
+			<td>
+				Email
+			</td>
+			<td>
+				{{$ibu->email}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				7.
+			</td>
+			<td>
+				Status Perkawinan
+			</td>
+			<td>
+				{{$ibu->status_perkawinan}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				8.
+			</td>
+			<td>
+				Status Hidup
+			</td>
+			<td>
+				{{$ibu->status_hidup}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				9.
+			</td>
+			<td>
+				NO HP / TELPON
+			</td>
+			<td>
+				{{$ibu->telepon}}
+			</td>
+		</tr>
+		<tr>
+			<td width="20px">
+				10.
+			</td>
+			<td>
+				Alamat
+			</td>
+			<td>
+				{{$ibu->alamat}}
+			</td>
+		</tr>
+		<tr>
+			<td colspan="3">
+				<b>ISTERI / SUAMI</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				1.
+			</td>
+			<td>
+				Nama
+			</td>
+			<td>
+				{{$pasangan->nama}}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				2.
+			</td>
+			<td>
+				Gelar Depan
+			</td>
+			<td>
+				{{$pasangan->gelar_depan}}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				3.
+			</td>
+			<td>
+				Gelar Belakang
+			</td>
+			<td>
+				{{$pasangan->gelar_belakang}}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				4.
+			</td>
+			<td>
+				Tanggal Lahir
+			</td>
+			<td>
+				{{indonesian_date($pasangan->tanggal_lahir)}}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				5.
+			</td>
+			<td>
+				Agama
+			</td>
+			<td>
+				{{$pasangan->agama}}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				6.
+			</td>
+			<td>
+				Email
+			</td>
+			<td>
+				{{$pasangan->email}}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				7.
+			</td>
+			<td>
+				Alamat
+			</td>
+			<td>
+				{{$pasangan->alamat}}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				8.
+			</td>
+			<td>
+				NO HP / TELPON
+			</td>
+			<td>
+				{{$pasangan->telepon}}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				9.
+			</td>
+			<td>
+				Status Perkawinan
+			</td>
+			<td>
+				{{$pasangan->status_perkawinan}}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				10.
+			</td>
+			<td>
+				Akter Perceraian
+			</td>
+			<td>
+				Nomor : {{$pasangan->akte_perceraian}}<br>
+				Tanggal : {{indonesian_date($pasangan->tanggal_akte_perceraian)}}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				11.
+			</td>
+			<td>
+				Akte Kelahiran
+			</td>
+			<td>
+				Nomor : {{$pasangan->akte_kelahiran}}<br>
+				Tanggal : {{indonesian_date($pasangan->tanggal_akte_perceraian)}}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				12.
+			</td>
+			<td>
+				Akte Meninggal
+			</td>
+			<td>
+				Nomor : {{$pasangan->akte_meninggal}}<br>
+				Tanggal : {{indonesian_date($pasangan->tanggal_akte_meninggal)}}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				13.
+			</td>
+			<td>
+				NPWP
+			</td>
+			<td>
+				Nomor : {{$pasangan->npwp}}<br>
+				Tanggal : {{indonesian_date($pasangan->tanggal_npwp)}}
+			</td>
+		</tr>
+		<tr>
+			<td align="center" valign="top">
+				14.
+			</td>
+			<td>
+				Jika Suami/Isteri Anda PNS, Tuliskan Nama
+			</td>
+			<td valign="top">
+				{{$pasangan->nama_pasangan}}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				15.
+			</td>
+			<td>
+				NIP Suami/Isteri Anda
+			</td>
+			<td>
+				{{$pasangan->nip_pasangan}}
+			</td>
+		</tr>
+		</table>
+		<table border="1"> 
+		<tr>
+			<td>
+				<b>ANAK</b><br>
+				<table border='1' width="100%">
+					<tr class="table_main_head">
+						<th></th>
+						<th>
+							Nama
+						</th>
+						<th>
+							Tempat Lahir
+						</th>
+						<th>
+							Tanggal Lahir
+						</th>
+						<th>
+							Jenis Kelamin
+						</th>
+						<th>
+							Pendidikan
+						</th>
+						<th>
+							Pekerjaan
+						</th>
+					</tr>
+					<tr>
+						<th align="center"></th>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+						<th>4</th>
+						<th>5</th>
+						<th>6</th>
+					</tr>
+					<?php $no = 1; ?>
+					@foreach($anak as $an)
+					<tr>
+						<td align="center">{{$no++}}</td>
+						<td>{{$an->nama}}</td>
+						<td></td>
+						<td>{{indonesian_date($an->tanggal_lahir)}}</td>
+						<td>{{$an->jenis_kelamin}}</td>
+						<td>{{$an->pendidikan}}</td>
+						<td>{{$an->pekerjaan}}</td>
+					</tr>
+					@endforeach
+				</table>
+				<b>(Sambungan Data Anak)</b>
+				<table border='1' width="100%">
+					<tr class="table_main_head">
+						<th rowspan="2"></th>
+						<th rowspan="2">
+							Agama
+						</th>
+						<th rowspan="2">
+							Status Perkawinan
+						</th>
+						<th>
+							Status Hidup
+						</th>
+						<th>
+							Status Anak
+						</th>
+						<th rowspan="2">
+							Alamat
+						</th>
+						<th rowspan="2">
+							NO TELPON/HP
+						</th>
+					</tr>
+					<tr class="table_main_head">
+						<th>
+							Hidup/Meninggal
+						</th>
+						<th>
+							AK/AA/AT
+						</th>
+					</tr>
+					<tr>
+						<th align="center"></th>
+						<th>7</th>
+						<th>8</th>
+						<th>9</th>
+						<th>10</th>
+						<th>11</th>
+						<th>12</th>
+					</tr>
+					<?php $no2 = 1; ?>
+					@foreach($anak as $an)
+					<tr>
+						<td align="center">{{$no2++}}</td>
+						<td>{{$an->agama}}</td>
+						<td>{{$an->status_perkawinan}}</td>
+						<td>{{$an->status_hidup}}</td>
+						<td>{{$an->status_anak}}</td>
+						<td>{{$an->alamat}}</td>
+						<td>{{$an->telepon}}</td>
+					</tr>
+					@endforeach
+				</table>
+				<br/>
+			</td>
+		</tr>
+	</table>
 	<br>
 	<h4><b>IV. RIWAYAT JABATAN </b></h4>
 	<table border="1">
@@ -196,11 +711,11 @@
 			<tr>
 				<td align="center">{{$no++}}</td>
 				<td>{{$rm->jabatan->title}}</td>
-				<td>{{$rm->tmt}} s/d </td>
+				<td>{{indonesian_date($rm->tmt)}} s/d </td>
 				<td>
 					Eselon  : {{$rm->eselon}}<br> 
 					No. SK : {{$rm->nomor_sk}}<br> 
-					Tgl SK : {{$rm->tanggal}}<br>
+					Tgl SK : {{indonesian_date($rm->tanggal)}}<br>
 					Tgl Sumpah: <br>
 					Nilai KUM  :
 				</td>
@@ -222,10 +737,10 @@
 			<tr>
 				<td>{{$norg}}</td>
 				<td>{{$rg->golongan->title}}</td>
-				<td>{{$rg->tmt}}</td>
+				<td>{{indonesian_date($rg->tmt)}}</td>
 				<td></td>
 				<td >{{$rg->nomor_sk}}<br>
-					{{$rg->tanggal_sk}}</td>
+					{{indonesian_date($rg->tanggal_sk)}}</td>
 			</tr>
 		<?php $norg++; ?>
 		@endforeach
@@ -295,7 +810,7 @@
 				<td>{{$ks->nama_kursus}}</td>
 				<td>{{$ks->penyelenggara}}</td>
 				<td>{{$ks->nomor_sertifikat}}</td>
-				<td>{{$ks->tanggal}}</td>
+				<td>{{indonesian_date($ks->tanggal)}}</td>
 				<td>{{$ks->jumlah_jam}}</td>
 			</tr>
 		@endforeach
@@ -319,7 +834,7 @@
 			<tr>
 				<td>{{$no++}}</td>
 				<td>{{$ph->nama_penghargaan}}<br> No. {{$ph->nomor_surat_keputusan}}</td>
-				<td>{{$ph->tanggal}}</td>
+				<td>{{indonesian_date($ph->tanggal)}}</td>
 				<td>{{$ph->nama_pemberi_penghargaan}}</td>
 			</tr>
 		@endforeach
