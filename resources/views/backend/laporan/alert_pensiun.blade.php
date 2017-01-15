b99fd4a9f183dbb55c28c1ab5f63396df688d546
Gede Lumbung <gedesumawijaya@gmail.com>
2017-01-15 20:49:56 +0800

    add ordering number & datatable

--- resources/views/backend/laporan/alert_pensiun.blade.php
+++ resources/views/backend/laporan/alert_pensiun.blade.php
@@ -3,21 +3,28 @@
 @section("title","Laporan Alert Pensiun")
 
 @section("content")
-<div class="table-responsive">
-	<table class="table table-bordered" style="zoom:80%">
-		<thead style="background:#eaeaea">
-			<tr>
-				<th width="180">NIP</th>
-				<th width="180">Nama</th>
-				<th>Jenis Jabatan</th>
-				<th>Kedudukan PNS</th>
-				<th>Umur</th>
-				<th>Tanggal Pensiun</th>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach($pegawai as $key=>$data)
+<div class="panel panel-featured panel-featured-primary">
+	<div class="panel-heading">
+		<h3 class="panel-title">@yield("title")</h3>
+	</div>
+	
+	<div class="panel-body">
+		<table class="table table-bordered" style="zoom:80%" id="table_id">
+			<thead style="background:#eaeaea">
 				<tr>
+					<th width="40">Nomor</th>
+					<th width="180">NIP</th>
+					<th width="180">Nama</th>
+					<th>Jenis Jabatan</th>
+					<th>Kedudukan PNS</th>
+					<th>Umur</th>
+					<th>Tanggal Pensiun</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($pegawai as $key=>$data)
+				<tr>
+					<td>{{$key+1}}</td>
 					<td>{{$data->nip}}</td>
 					<td>{{($data->gelar_depan != '-' ? $data->gelar_depan : '')}} {{$data->nama_lengkap}}{{($data->gelar_belakang != '' ? ',' : '')}} {{($data->gelar_belakang != '' ? $data->gelar_belakang : '')}}</td>
 					<td>{{$data->jenis_jabatan}}</td>
@@ -31,9 +38,20 @@
 						{{indonesian_date($data->pensiun)}}
 					</td>
 				</tr>
-			@endforeach
-		</tbody>
-	</table>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
 </div>
 
 @endsection
+
+@push("script")
+
+<script type="text/javascript">
+	$(document).ready( function () {
+		$('#table_id').DataTable();
+	} );
+</script>
+
+@endpush
